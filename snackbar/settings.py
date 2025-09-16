103841f32fb14a8bb22d318ee03ffc13d05335e8
mauricebore <mauricebore22@gmail.com>
2025-09-16 12:54:50 +0000

    Initial commit - Snackbar Django project

--- snackbar/settings.py
+++ snackbar/settings.py
@@ -144,3 +144,14 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Where collected files will be stored (when running collectstatic)
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
+
+import os
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "main" / "static"]   # app-level static
+STATIC_ROOT = BASE_DIR / "staticfiles"              # collected here
+
